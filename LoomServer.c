22d0ddb27bbb7a1c93a2d37c2b204e810884686f
Andrew Collins <collinan@oregonstate.edu>
2018-12-16 18:32:49 -0800

    Initial commit

---
+++ LoomServer.c
@@ -0,0 +1,18 @@
+#include <stdio.h> /*for printf and fprintf*/
+#include <sys/socket.h> /*for socket(), bind(), connect(), recv(), send() */
+#include <arpa/inet.h> /*for sockaddr_in and inet_ntoa*/
+#include <stdlib.h> /*for atoi() and exit() */
+#include <string.h> /*for memset() */
+#include <unistd.h> /* for close() */
+
+int CreateTCPServerSocket(unsigned short ServerPort);
+
+int main(int argc, char *argv[]){
+
+
+
+
+
+	return 0;
+
+}
