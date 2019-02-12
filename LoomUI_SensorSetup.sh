57bf304a503d0e79a6382bcf447255d5cddecac0
Andrew <collinan@oreongstate.edu>
2019-02-12 03:37:52 -0800

    getting usen imput complete

--- LoomUI_SensorSetup.sh
+++ LoomUI_SensorSetup.sh
@@ -2,33 +2,22 @@
 
 #give program permision initially "chmod +x LoomUI_Setup.sh"
 
-echo Tasks:
-echo 0-add sensor
-echo 1-remove sensor
-echo 2-quit
+checkLISTofSensors(){
 
-read -p 'Enter the number for Task: ' userin
-echo ${#userin}
-if [ $userin -eq 0 ]
-then
-	echo add sensor
-	add_sensor()
+while [ test $ = $(cat -e $SensorList | tail -n 1) ]#check if last line in file in newline
+do
+sed '$d' $SensorList
+done
 
-elif [ $userin -eq 1 ]
-then
-	echo remove sensor
-	remove_sensor()
 
-elif [ $userin -eq 2 ]
-then
-	echo quit
-	exit 0
-else
-	echo invalid input
-fi
+}
 
+#adding sensor function:
 add_sensor(){
 
+while :
+do
+echo
 echo Avaliable sensors:
 echo 0-Salinity sensor
 echo 1-PH Sensor
@@ -36,7 +25,9 @@ echo 2-Hall Effect Sensor
 echo 3-Flow Meter
 echo Enter q to quit.
 	
+
 read -p 'Enter the sensor number: ' sensornum
+
 
 case $sensornum in 
 	0)
@@ -44,24 +35,64 @@ case $sensornum in
 	;;
 	1)
 	echo Adding PH Sensor
+	if[ test $ = $(cat -e $SensorList | tail -n 1) ]
 	;;
 	2)
 	echo Adding Hall Effect Sensor
+	if[ test $ = $(cat -e $SensorList | tail -n 1) ]
 	;;
 	3)
 	echo Adding Flow Meter
+	if[ test $ = $(cat -e $SensorList | tail -n 1) ]	
 	;;
 	q)
 	echo Quitting
 	exit 0
+	;;
 	*)
 	echo invalid input
 	;;
 esac
+done
 
 }
 
+
+#removing sensor function
 remove_sensor(){
 
-
+echo returning sensor
 }
+
+SensorList=ListOfSensors.txt
+
+
+while :
+do
+echo 
+echo Tasks:
+echo 0-add sensor
+echo 1-remove sensor
+echo 2-quit
+
+read -p 'Enter the number for Task: ' userin
+echo ${#userin} #number of argument entered
+
+if [ "$userin" = "0" ]
+then
+	echo add sensor
+	add_sensor
+elif [ "$userin" = "1" ]
+then
+	echo remove sensor
+	remove_sensor
+elif [ "$userin" = "2" ]
+then
+	echo quit
+	exit 0
+else
+	echo invalid input
+fi
+
+done
+
