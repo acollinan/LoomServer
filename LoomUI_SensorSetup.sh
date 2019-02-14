300ab262ec1ac2e43c768ac94c506978e751185b
Andrew <collinan@oreongstate.edu>
2019-02-14 02:27:35 -0800

    Updated UI

--- LoomUI_SensorSetup.sh
+++ LoomUI_SensorSetup.sh
@@ -6,14 +6,10 @@
 
 CreateSensorData_File(){
 
-echo
 echo Creating sensor Data file ./$4/SensorData$3.csv for $1 $2 Id: $3
 #check if file already exists
 echo This is the Data file for the $1 $2 ID : $3  > "./SensorFiles/$4/SensorData$3.csv"
-echo Directory Tree:After Data file
-find SensorFiles -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'
-echo
-cat ./SensorFiles/$4/SensorData$3.csv
+#cat ./SensorFiles/$4/SensorData$3.csv
 
 #push colum headers to file
 
@@ -21,15 +17,11 @@ cat ./SensorFiles/$4/SensorData$3.csv
 
 CreateSensorConfig_File(){
 
-echo
 echo Creating sensor config file ./$4/SensorConfig$3.txt for $1 $2 Id: $3
 #check if file already exists
 echo This is the configuration file for the $1 $2 ID : $3  > "./SensorFiles/$4/SensorConfig$3.txt"
-echo Directory Tree:After Config file
-find SensorFiles -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'
-echo
-cat ./SensorFiles/$4/SensorConfig$3.txt
-#push colum headers to file
+#cat ./SensorFiles/$4/SensorConfig$3.txt
+#--------push colum headers to file
 
 
 }
@@ -37,42 +29,43 @@ cat ./SensorFiles/$4/SensorConfig$3.txt
 CreateSensor_directory(){
 
 #check if directory already exists
-echo
 echo Creating directory Sensor$3 for $1 $2 Id: $3
 mkdir "./SensorFiles/Sensor$3"
 
-echo Directory Tree
-find SensorFiles -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'
-echo
 
 CreateSensorConfig_File $1 $2 $3 "Sensor$3" 
 CreateSensorData_File $1 $2 $3 "Sensor$3" 
+echo
+echo Directory Tree
+find SensorFiles -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'
+echo
 }
 
 AddtoLISTofSensors(){
 
 SensorList=./SensorFiles/ListOfSensors.txt
-echo $SensorList
+#echo $SensorList
 
-echo In list sensors
-cat $SensorList
+#cat $SensorList
 #cat ./SensorFiles/ListOfSensors.txt
-cat -e $SensorList | tail -n 1 #check if last line in file in newline
+#cat -e $SensorList | tail -n 1 #check if last line in file in newline
 #while [ $(test $ = $(cat -e $SensorList | tail -n 1)) ; echo $? ] #check if last line in file in newline
 #do
 #sed -i '' -e '$ d' $SensorList
 #done
-echo $SensorList
+
+#-----neeed to add logic if 0 sensors in file
+#echo $SensorList
 
 MaxSensorNum=$(tail -n 1 $SensorList | cut -d ' ' -f 1)
-echo MaxSensorNum: $MaxSensorNum
+#echo MaxSensorNum: $MaxSensorNum
 
 #do check before assignment
 
 NewSensorNum=$(($MaxSensorNum + 1))
-echo NewSensorNum: $NewSensorNum
+#echo NewSensorNum: $NewSensorNum
 
-echo $NewSensorNum $1 $2 $(date) 
+#echo $NewSensorNum $1 $2 $(date) 
 echo $NewSensorNum $1 $2 $(date) >> $SensorList
 
 
@@ -92,14 +85,14 @@ echo
 echo Avaliable sensors:
 echo 0-Salinity sensor
 echo 1-PH Sensor
-echo 2-Hall Effect Sensor
-echo 3-Flow Meter
+echo 2-Hall-Effect Sensor
+echo 3-Flow Sensor
 echo Enter q to quit.
 	
 
 read -p 'Enter the sensor number: ' sensornum
 
-
+echo
 #Edit code below to add new sensor, follow format below
 case $sensornum in 
 	0)
@@ -113,12 +106,12 @@ case $sensornum in
 	break
 	;;
 	2)
-	echo Adding Hall Effect Sensor
-	AddtoLISTofSensors PH Sensor
+	echo Adding Hall-Effect Sensor
+	AddtoLISTofSensors Hall-Effect Sensor
 	break
 	;;
 	3)
-	echo Adding Flow Meter
+	echo Adding Flow Sensor
 	AddtoLISTofSensors Flow Sensor
 	break
 	;;
@@ -138,8 +131,44 @@ done
 
 #removing sensor function
 remove_sensor(){
+cat ./SensorFiles/ListOfSensors.txt
+echo Enter q to quit.
 
-echo returning sensor
+read -p 'From list of sensors above, enter sensor ID to remove: ' sensorid
+
+if [ "$sensorid" = "q" ]
+then
+	echo quiting
+	clear
+	exit 0 
+fi
+if [ "$sensorid" = "111000" ]
+then
+	echo This is not a sensor that can be deleted.
+elif [ -d "./SensorFiles/Sensor$sensorid" ]
+then
+	echo Are you sure you want to delete the following files:
+	find ./SensorFiles/Sensor$sensorid -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'
+	read -p 'Enter y or n: ' userinys
+	if [ "$userinys" = "y" ]
+	then
+		sed -i '' "/$sensorid/d" ./SensorFiles/ListOfSensors.txt
+		rm -fr ./SensorFiles/Sensor$sensorid
+		clear
+	elif [ "$useinys" = "n" ]
+	then
+		echo
+		clear 	
+	else
+		echo Error:Invalid input.
+	fi
+		
+else
+	echo Error: Invalid Sensor id
+fi
+
+
+
 }
 
 
@@ -154,6 +183,8 @@ echo Enter q to quit.
 
 read -p 'Enter the number for Task: ' userin
 echo ${#userin} #number of argument entered
+
+clear
 
 if [ "$userin" = "0" ]
 then
